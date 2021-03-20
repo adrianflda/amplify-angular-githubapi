07c2eafd581c9bcf2c53347feb16e20fcf9f053e
adrian <adrian.flda@gmail.com>
2021-03-19 22:31:09 -0400

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GithubCommitsComponent } from './github-commits/github-commits.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubApiComponent } from './github-api/github-api.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'github-commits', component: GithubCommitsComponent },
+  { path: 'github-profile', component: GithubProfileComponent },
+  { path: 'github-api', component: GithubApiComponent },
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { GithubCommitsComponent } from './github-commits/github-commits.component';
+import { GithubProfileComponent } from './github-profile/github-profile.component';
+import { GithubApiComponent } from './github-api/github-api.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GithubCommitsComponent,
+    GithubProfileComponent,
+    GithubApiComponent,
+    ToolbarComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatIconModule, MatToolbarModule, MatDividerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
